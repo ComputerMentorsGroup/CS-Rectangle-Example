b5709b6bc738da0bf2e231a952fcf7e59b1e35c7
Chris Willingham <Chris Willingham>
2017-03-31 12:06:27 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string ReadInput(string query)
+        {
+            Console.Write(query + ": ");
+            return Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            var rectangle = new Rectangle();
+            while (true)
+            {
+                try
+                {
+                    rectangle.Width = float.Parse(ReadInput("Width"));
+                    rectangle.Height = float.Parse(ReadInput("Height"));
+                    Console.WriteLine("The area of the rectangle is " + rectangle.Area);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rectangle
+    {
+        private float _width;
+        private float _height;
+
+        public float Width {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value > 0)
+                    _width = value;
+                else
+                    throw new FormatException();
+            }
+        }
+        public float Height
+        {
+            get => _height;
+            set
+            {
+                if (value > 0)
+                    _height = value;
+                else
+                    throw new FormatException();
+            }
+        }
+
+        public float Area
+        {
+            get
+            {
+                return _width * _height;
+            }
+        }
+
+
+    }
+}
